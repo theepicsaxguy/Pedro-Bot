e54b6ac2b822dffe925a1c593feb0e4ada1f1672
Benjamin Sandén <benjamin.sanden@hotmail.com>
2025-06-10 17:45:15 +0200

    feat(levels): add challenges, leaderboard, xp decay

--- config/constants.js
+++ config/constants.js
@@ -5,9 +5,14 @@ module.exports = {
       LEAVE: 'leave',
       // Add more button IDs as needed
     },
-    PERMISSIONS: {
-      ADMINISTRATOR: 'ADMINISTRATOR',
-      // Add more permissions as needed
-    },
-  };
+  PERMISSIONS: {
+    ADMINISTRATOR: 'ADMINISTRATOR',
+    // Add more permissions as needed
+  },
+  XP_MULTIPLIERS: {
+    MESSAGE: 1,
+    DAILY: 2,
+    WEEKLY: 5,
+  },
+};
   
